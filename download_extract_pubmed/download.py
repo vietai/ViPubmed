29e8e8757d79aba7312531e2e0b1801aa10254e8
justinphan3110 <justinphan3110@gmail.com>
2023-02-02 18:32:53 -0500

    init vipubmed t5x

--- download_extract_pubmed/download.py
+++
@@ -1,6 +0,0 @@
-import os
-files_name = [f'pubmed22n{str(i).zfill(4)}.xml.gz' for i in range(1,1115)]
-
-for file_name in files_name:
-  os.system(f'wget https://ftp.ncbi.nlm.nih.gov/pubmed/baseline/{file_name}')
-  os.system(f'gzip -d {file_name}')
